2c6d5ae41e3e252d03b49e64964fca78744d3c92
lxfly2000 <lxfly2000@outlook.com>
2023-03-07 21:34:16 +0800

    Remove unused files.

--- buildexe.bat
+++ buildexe.bat
@@ -1,3 +1,3 @@
-py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyopenssl tld dnspython tornado pyinstaller
+py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyopenssl tld dnspython pyinstaller
 py -m PyInstaller accesser.spec
 copy /Y dist\accesser.exe D:\Accesser
