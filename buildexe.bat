f51b5d8faec4015187e1d72361a229b73aee2c6c
Yuxin <lxfly2000@outlook.com>
2023-12-10 12:19:34 +0800

    Update

--- buildexe.bat
+++ buildexe.bat
@@ -1,6 +1,6 @@
 curl http://localhost:7654/shutdown
 rd /s /q build
 rd /s /q dist
-py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple pyopenssl tld "dnspython<2.4" pyinstaller httpx[http2]
+py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple "pyopenssl<23.3.0" tld "dnspython<2.4" pyinstaller httpx[http2] "tomli>=1.1.0"
 py -m PyInstaller accesser.spec
 copy /Y dist\accesser.exe D:\Accesser
