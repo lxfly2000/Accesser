71188fa92e4b7564199fe5837d0d7f063971f018
Yuxin <lxfly2000@outlook.com>
2024-04-20 15:01:39 +0800

    Update.

--- buildexe.bat
+++ buildexe.bat
@@ -1,6 +1,7 @@
 curl http://localhost:7654/shutdown
 rd /s /q build
 rd /s /q dist
+py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple --upgrade pip
 py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple -r requirements.txt
 py -m pip install -i https://pypi.tuna.tsinghua.edu.cn/simple "pyopenssl<23.3.0" pyinstaller httpx[http2] httpx[brotli] httpx[socks]
 py -m PyInstaller accesser.spec
